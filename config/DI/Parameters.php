1a9e8ce3749635728a26a39db7e481abb4631379
Kernel Panic <jgg.jobs.development@gmail.com>
2021-06-02 00:07:21 +0200

    Add doctrine parameters

--- config/DI/Parameters.php
+++ config/DI/Parameters.php
@@ -6,7 +6,20 @@ use DI\ContainerBuilder;
 return static function(ContainerBuilder $containerBuilder): void {
     $containerBuilder->addDefinitions(definitions: array(
        'parameters' => array(
-           'environment_mode' => $_ENV['ENVIRONMENT_MODE']
+           'environment_mode' => $_ENV['ENVIRONMENT_MODE'],
+           'doctrine' => array(
+               'mapping_files' => __DIR__ . '/../ORM/mapping',
+               'domain_files' => __DIR__ . '/../../src/Domain',
+               'database' => array(
+                   'driver' => '',
+                   'charset' => 'utf8',
+                   'port' => $_ENV['DB_PORT'],
+                   'host' => $_ENV['DB_HOST'],
+                   'user' => $_ENV['DB_USER'],
+                   'password' => $_ENV['DB_PASSWORD'],
+                   'name' => $_ENV['DB_NAME']
+               )
+           )
        )
     ));
 };
