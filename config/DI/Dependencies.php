b236bcf9f55196ad874bca1581145a3ee570d884
KernelPanic <kernelpanic0x000007C4@protonmail.com>
2021-11-19 18:42:35 +0100

    JMS Serializer.

--- config/DI/Dependencies.php
+++ config/DI/Dependencies.php
@@ -7,6 +7,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Driver\XmlDriver;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\Persistence\Mapping\Driver\FileDriver;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -53,6 +56,16 @@ return static function(ContainerBuilder $containerBuilder): void {
             $logger->pushHandler(handler: $handler);
 
             return $logger;
+        },
+
+        Serializer::class => static function (ContainerInterface $container): Serializer {
+            return SerializerBuilder::create()
+                ->setSerializationContextFactory(static function() {
+                    return SerializationContext::create()
+                        ->setSerializeNull(bool: true);
+                })
+                ->addMetadataDir(dir: __DIR__ . '/../JMS')
+                ->build();
         }
     );
     $containerBuilder->addDefinitions($definitions);
